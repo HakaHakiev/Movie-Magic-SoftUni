d1091eaed3ebec32a71c4999e7f744d6473d9789
HakaHakiev <122813219+HakaHakiev@users.noreply.github.com>
2024-01-21 23:30:50 +0200

    Fix path issue

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const router = require("./routes");
 const configHandlebars = require("./config/configHandlebars");
+const configExpress = require("./config/configExpress");
 
 const app = express();
 const port = 3000;
 
 configHandlebars(app);
+configExpress(app);
 
 app.use(router);
 
