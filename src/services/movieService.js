3af3b56c21e888a718eaa0a9f0e9e6b21c457ed0
HakaHakiev <122813219+HakaHakiev@users.noreply.github.com>
2024-01-23 23:02:37 +0200

    Show dynamic movies on search page

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -5,7 +5,7 @@ const movies = [
     genre: "Adventure",
     director: "Pedri",
     year: "2020",
-    imageUrl: "img/jungle-cruise.jpeg",
+    imageUrl: "/img/jungle-cruise.jpeg",
     rating: "4",
     description:
       "reaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world.",
