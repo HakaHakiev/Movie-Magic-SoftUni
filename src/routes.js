19c472a69178731fefc60e30f1d949ccb3176c22
HakaHakiev <122813219+HakaHakiev@users.noreply.github.com>
2024-01-30 00:00:39 +0200

    Add create cast page

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,9 @@ const router = require("express").Router();
 
 const homeController = require("./controllers/homeController");
 const movieController = require("./controllers/movieController");
+const castController = require("./controllers/castController");
 
+router.use("/cast", castController);
 router.use(movieController);
 router.use(homeController);
 
