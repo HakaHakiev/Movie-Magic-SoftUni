835ef4534f7e5ee027aaf7168f0bd165a45df36c
HakaHakiev <122813219+HakaHakiev@users.noreply.github.com>
2024-01-29 23:37:52 +0200

    Refactor movieService to use Movie model

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,48 +1,17 @@
 const Movie = require("../models/Movie");
 
-const movies = [
-  {
-    _id: 1,
-    title: "Jungle Cruise",
-    genre: "Adventure",
-    director: "Pedri",
-    year: "2020",
-    imageUrl: "/img/jungle-cruise.jpeg",
-    rating: "4",
-    description:
-      "Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world.",
-  },
-  {
-    _id: 2,
-    title: "The Little Mermaid",
-    genre: "Fantasy",
-    director: "Spielberg",
-    year: "2017",
-    imageUrl: "/img/the-little-mermaid.jpg",
-    rating: "3",
-    description:
-      "The youngest of King Triton's daughters, Ariel is a beautiful and spirited young mermaid with a thirst for adventure.",
-  },
-  {
-    _id: 3,
-    title: "Home Alone",
-    genre: "Comedy",
-    director: "Robert",
-    year: "2004",
-    imageUrl: "/img/home-alone.jpeg",
-    rating: "5",
-    description:
-      "It is Christmas time and the McCallister family is preparing for a vacation in Paris, France. ",
-  },
-];
-
 exports.getAll = () => {
   // return [...movies];
-  return movies.slice();
+  // return movies.slice();
+  const movies = Movie.find();
+
+  return movies;
 };
 
-exports.search = (title, genre, year) => {
-  let result = movies.slice();
+// TODO: Filter result in MongoDB
+exports.search = async (title, genre, year) => {
+  // let result = movies.slice();
+  let result = await Movie.find().lean();
 
   if (title) {
     result = result.filter((movie) =>
@@ -63,12 +32,6 @@ exports.search = (title, genre, year) => {
   return result;
 };
 
-exports.getOne = (movieId) => {
-  const movie = movies.find((movie) => movie._id == movieId);
+exports.getOne = (movieId) => Movie.findById(movieId); // movies.find((movie) => movie._id == movieId)
 
-  return movie;
-};
-
-exports.create = (movieData) => {
-  return Movie.create(movieData);
-};
+exports.create = (movieData) => Movie.create(movieData);
