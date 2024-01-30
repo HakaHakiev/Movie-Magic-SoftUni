c9d31562e80b9a78c00f5ab7fb03d3e881c885f2
HakaHakiev <122813219+HakaHakiev@users.noreply.github.com>
2024-01-30 17:21:50 +0200

    Create cast model

---
+++ src/models/Cast.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const castSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+    min: 14,
+    max: 120,
+  },
+  born: {
+    type: String,
+    required: true,
+  },
+  nameInMovie: {
+    type: String,
+    required: true,
+  },
+  castImage: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return /^https?\/\//.test(value);
+      },
+      message: (props) => `${props.value} is invalid url for castImage!`,
+    },
+  },
+});
+
+const Cast = mongoose.model("Cast", castSchema);
+
+module.exports = Cast;
