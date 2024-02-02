ca840274e632de757b1a35de2ea4120b97af2a0c
HakaHakiev <122813219+HakaHakiev@users.noreply.github.com>
2024-02-02 18:52:26 +0200

    Fix search page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -12,10 +12,10 @@ router.get("/about", (req, res) => {
   res.render("about");
 });
 
-router.get("/search", (req, res) => {
+router.get("/search", async (req, res) => {
   // console.log(req.query);
   const { title, genre, year } = req.query;
-  const movieResult = movieService.search(title, genre, year);
+  const movieResult = await movieService.search(title, genre, year).lean();
 
   res.render("search", { movies: movieResult, title, genre, year });
 });
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -10,27 +10,26 @@ exports.getAll = () => {
 };
 
 // TODO: Filter result in MongoDB
-exports.search = async (title, genre, year) => {
-  // let result = movies.slice();
-  let result = await Movie.find().lean();
+exports.search = (title, genre, year) => {
+  // let result = await Movie.find().lean();
+  let query = Movie.find();
 
   if (title) {
-    result = result.filter((movie) =>
-      movie.title.toLowerCase().includes(title.toLowerCase())
-    );
+    // result = result.filter((movie) => movie.title.toLowerCase().includes(title.toLowerCase()));
+    query = query.find({ title: new RegExp(title, "i") });
   }
 
   if (genre) {
-    result = result.filter(
-      (movie) => movie.genre.toLowerCase() === genre.toLowerCase()
-    );
+    // result = result.filter((movie) => movie.genre.toLowerCase() === genre.toLowerCase());
+    query = query.find({ genre: genre.toLowerCase() });
   }
 
   if (year) {
-    result = result.filter((movie) => movie.year === year);
+    // result = result.filter((movie) => movie.year === year);
+    query = query.find({ year });
   }
 
-  return result;
+  return query;
 };
 
 exports.getOne = (movieId) => Movie.findById(movieId).populate("casts"); // movies.find((movie) => movie._id == movieId)
