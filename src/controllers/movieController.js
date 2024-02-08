54cbd7a3f78f8037e37a15b3759d502608383b8b
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-02-08 23:17:06 +0200

    Add conditional rendering of details buttons

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -31,13 +31,14 @@ router.get("/movies/:movieId", isAuth, async (req, res) => {
   const movieId = req.params.movieId;
   const movie = await movieService.getOne(movieId).lean();
   // const casts = await castService.getByIds(movie.casts).lean();
+  const isOwner = movie.owner == req.user._id;
 
   // TODO: This is not perfect, use handlebars helpers
   // movie.rating = new Array(Number(movie.rating)).fill(true);
   movie.ratingStars = "&#x2605".repeat(movie.rating);
 
   // console.log(movie);
-  res.render("movie/details", { movie });
+  res.render("movie/details", { movie, isOwner });
 });
 
 router.get("/movies/:movieId/attach", async (req, res) => {
